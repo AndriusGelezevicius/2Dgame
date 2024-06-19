59e8681a1302166521e45b24a164dfbde03dec03
AndriusGelezevicius <andriusgelezevicius@gmail.com>
2024-06-19 20:03:39 +0300

    Add class Player

--- Main.py
+++ Main.py
@@ -1,11 +1,13 @@
 import pygame
+from Player import Player
+
 
 pygame.init()
 clock = pygame.time.Clock()
 
 screen_width = 1000
-screen_hight = 600
-screen = pygame.display.set_mode((screen_width, screen_hight))
+screen_height = 600
+screen = pygame.display.set_mode((screen_width, screen_height))
 pygame.display.set_caption("2Dgame")
 
 while True:
