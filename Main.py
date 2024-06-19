3300a3d238d2973444f1853e860c0a056e0b9222
AndriusGelezevicius <andriusgelezevicius@gmail.com>
2024-06-19 21:00:00 +0300

    add player_down image

--- Main.py
+++ Main.py
@@ -10,12 +10,22 @@ screen_height = 600
 screen = pygame.display.set_mode((screen_width, screen_height))
 pygame.display.set_caption("2Dgame")
 
+# Load sprite Sheets //:TODO: add more images
+player_sheet = pygame.image.load("Images/rpg_sprite_walk_down.png").convert_alpha()
+player = Player(player_sheet)
+
+
+
+
+
+
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
+            exit()
 
-
-screen.fill((0, 255, 0))
-pygame.display.flip()
-clock.tick(60)
+    screen.fill((0, 255, 0))
+    screen.blit(player.image, (100,100))
+    pygame.display.flip()
+    clock.tick(60)
--- Player.py
+++ Player.py
@@ -2,11 +2,13 @@ import pygame
 
 class Player(pygame.sprite.Sprite):
     def __init__(self, image):
+        super().__init__()
         self.sheet = image
-
+        self.image = self.sheet # initial image
+        self.rect = self.image.get_rect()
     def get_image(self, frame, width, height, scale, colour):
         image = pygame.Surface((width, height), pygame.SRCALPHA).convert_alpha()
-        image.blit(self.sheet, (0 ,0), (frame * width), 0, width, height)
+        image.blit(self.sheet, (0, 0), (frame * width), 0, width, height)
         image = pygame.transform.scale(image, (image * scale, height * scale))
         image.set_colorkey(colour)
         return image
